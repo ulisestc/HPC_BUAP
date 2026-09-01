e3e83ac6291ca63f95f39143daf58dd2616a985a
ulisestc <ulisestc27@gmail.com>
2026-09-01 01:49:17 -0600

    practica 2: productor consumidor en C con mutex y semaforos

--- practica2/practica2.c
+++ practica2/practica2.c
@@ -11,12 +11,96 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
+#define SIZE 10
 
 // Cola circular
+// hacemos la lista circular al usar in = (in + 1) % SIZE y out = (out + 1) % SIZE, siempre iterará del 0 a SIZE-1
+int queue[SIZE];
+int in = 0;
+int out = 0;
 
+pthread_mutex_t mutex; // para bloquear arreglo de la cola
+sem_t sem_empty; // indicará espacios vacíos en la cola
+sem_t sem_full; // indicará elementos no vacíos en la cola
 
+// PRODUCTOR
+void* producer(void* arg)
+{
+    while(1) // inifinito
+    {
+        // espera a que haya > 0 espacios vacíos (Para poder producir), cuando entra, 
+        // resta 1 (un espacio vacío menos)
+        sem_wait(&sem_empty);
 
+        //activar mutex y bloquear acceso
+        pthread_mutex_lock(&mutex);
 
+        //generar dato aleatorio e introducirlo
+        int dato = rand() % 100;
+        queue[in] = dato;
+        printf("Productor produjo: %d en posicion %d\n", dato, in);
+        
+        // sumar a indice de inserción
+        in = (in + 1) % SIZE;
 
+        pthread_mutex_unlock(&mutex); // desbloquea cola
 
+        sem_post(&sem_full); // hay +1 lugares llenos
+    }
+    return NULL;
+}
+
+void* consumer(void* arg)
+{
+    //saber que consumidor es
+    int id = *((int*)arg);
+
+    while (1)
+    {
+        sem_wait(&sem_full); // espera a que haya  > 0 productos para consumir
+
+        //bloquear mutex
+        pthread_mutex_lock(&mutex);
+
+        // consumir
+        int dato = queue[out];
+        printf("Consumidor %d extrajo: %d de posicion %d\n", id, dato, out);    
+
+        // sumar indice de extracción
+        out = (out + 1) % SIZE;
+
+        pthread_mutex_unlock(&mutex); // desbloquear mutex
+
+        sem_post(&sem_empty); // +1 lugares libres!
+    }
+    return NULL;
+}
+
+int main()
+{
+    pthread_t hilos[3];
+
+    int id1 = 1;
+    int id2 = 2;
+
+    pthread_mutex_init(&mutex, NULL);
+    sem_init(&sem_empty, 0, SIZE); // inicializar semáforo de espacios vacíos con valor SIZE
+    sem_init(&sem_full, 0, 0);
+
+    pthread_create(&hilos[0], NULL, producer, NULL);
+    pthread_create(&hilos[1], NULL, consumer, &id1);
+    pthread_create(&hilos[2], NULL, consumer, &id2);
+
+    // Esperar a que todos los hilos terminen
+    for (int i = 0; i < 3; i++) {
+        pthread_join(hilos[i], NULL);
+    }
+
+    
+    pthread_mutex_destroy(&mutex);
+    sem_destroy(&sem_empty);
+    sem_destroy(&sem_full);
+
+    return 0;
+}
 
